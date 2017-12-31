33cd71e77821ce0cfb36cf47102d17547e1f441f
johnrhampton <johnrhampton@gmail.com>
2017-12-31 16:17:50 -0500

    migrating to new patterns

--- main.js
+++ main.js
@@ -1,107 +1,119 @@
-const path = require('path')
-const glob = require('glob')
-const electron = require('electron')
-const autoUpdater = require('./auto-updater')
+const path = require('path');
+const glob = require('glob');
+const electron = require('electron');
 
-const BrowserWindow = electron.BrowserWindow
-const app = electron.app
+const autoUpdater = require('./auto-updater');
 
-const debug = /--debug/.test(process.argv[2])
+const BrowserWindow = electron.BrowserWindow;
+const app = electron.app;
 
-if (process.mas) app.setName('Electron APIs')
+const debug = /--debug/.test(process.argv[2]);
 
-var mainWindow = null
+if (process.mas) app.setName('Azure Dashboard');
 
-function initialize () {
-  var shouldQuit = makeSingleInstance()
-  if (shouldQuit) return app.quit()
+let mainWindow = null;
 
-  loadDemos()
+/**
+ * Make this app a single instance app.
+ * The main window will be restored and focused instead of a second window
+ * opened when a person attempts to launch a second instance.
+ * Returns true if the current version of the app should quit instead of
+ * launching.
+ * @return {boolean}
+ */
+function makeSingleInstance() {
+  if (process.mas) return false;
 
-  function createWindow () {
-    var windowOptions = {
+  return app.makeSingleInstance(() => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
+}
+
+/**
+ * Require JS files in the main-process dir
+ */
+function loadMainProcessFiles() {
+  let files = glob.sync(path.join(__dirname, 'main-process/**/*.js'));
+  files.forEach(file => {
+    require(file);
+  });
+  autoUpdater.updateMenu();
+}
+
+/**
+ * Initialize the Electron App
+ */
+function initialize() {
+  let shouldQuit = makeSingleInstance();
+  if (shouldQuit) return app.quit();
+
+  loadMainProcessFiles();
+
+  // create the main window
+  function createWindow() {
+    let windowOptions = {
       width: 1080,
       minWidth: 680,
       height: 840,
       title: app.getName()
-    }
+    };
 
     if (process.platform === 'linux') {
-      windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
+      windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png');
     }
 
-    mainWindow = new BrowserWindow(windowOptions)
-    mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
+    mainWindow = new BrowserWindow(windowOptions);
+    mainWindow.loadURL(path.join('file://', __dirname, '/index.html'));
 
     // Launch fullscreen with DevTools open, usage: npm run debug
     if (debug) {
-      mainWindow.webContents.openDevTools()
-      mainWindow.maximize()
-      require('devtron').install()
+      mainWindow.webContents.openDevTools();
+      mainWindow.maximize();
+      require('devtron').install();
     }
 
-    mainWindow.on('closed', function () {
-      mainWindow = null
-    })
+    mainWindow.on('closed', function() {
+      mainWindow = null;
+    });
   }
 
-  app.on('ready', function () {
-    createWindow()
-    autoUpdater.initialize()
-  })
+  app.on('ready', function() {
+    createWindow();
+    autoUpdater.initialize();
+  });
 
-  app.on('window-all-closed', function () {
+  app.on('window-all-closed', function() {
     if (process.platform !== 'darwin') {
-      app.quit()
+      app.quit();
     }
-  })
+  });
 
-  app.on('activate', function () {
+  app.on('activate', function() {
     if (mainWindow === null) {
-      createWindow()
+      createWindow();
     }
-  })
-}
-
-// Make this app a single instance app.
-//
-// The main window will be restored and focused instead of a second window
-// opened when a person attempts to launch a second instance.
-//
-// Returns true if the current version of the app should quit instead of
-// launching.
-function makeSingleInstance () {
-  if (process.mas) return false
-
-  return app.makeSingleInstance(function () {
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore()
-      mainWindow.focus()
-    }
-  })
-}
-
-// Require each JS file in the main-process dir
-function loadDemos () {
-  var files = glob.sync(path.join(__dirname, 'main-process/**/*.js'))
-  files.forEach(function (file) {
-    require(file)
-  })
-  autoUpdater.updateMenu()
+  });
 }
 
 // Handle Squirrel on Windows startup events
 switch (process.argv[1]) {
   case '--squirrel-install':
-    autoUpdater.createShortcut(function () { app.quit() })
-    break
+    autoUpdater.createShortcut(function() {
+      app.quit();
+    });
+    break;
   case '--squirrel-uninstall':
-    autoUpdater.removeShortcut(function () { app.quit() })
-    break
+    autoUpdater.removeShortcut(function() {
+      app.quit();
+    });
+    break;
   case '--squirrel-obsolete':
   case '--squirrel-updated':
-    app.quit()
-    break
+    app.quit();
+    break;
   default:
-    initialize()
+    initialize();
 }
